1e4b5fdda7c2c5d66db003183c8284247d18b538
GozdeOrgu <33121624+GozdeOrgu@users.noreply.github.com>
2021-09-24 11:58:11 +0300

    KUTUPHANE-SISTEMI-PROJESI

---
+++ Kutuphane/Kutuphane/SQLBaglanti.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Kutuphane
+{
+    public class SQLBaglanti
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=DESKTOP-HCB46VG\SQLEXPRESS;Initial Catalog=Kutuphane;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
